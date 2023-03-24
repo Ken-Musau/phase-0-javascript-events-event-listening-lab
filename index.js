7a375dd5d89aadff120fd6ecd25b7a95d630727e
Kennedy Musau <kennedy.musauu@gmail.com>
2023-03-24 21:58:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-function addingEventListener() {
+function clickAlert() {
+  alert("Hee hee, that tickles!");
 }
+
+const input = document.getElementById("input");
+function addingEventListener() {
+  input.addEventListener("click", clickAlert);
+}
+
+const main = document.getElementById("main");
+main.addEventListener("click", clickAlert);
+
+addingEventListener();
